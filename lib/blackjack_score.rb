9de961938a9edf12575ad6b1277d8864e3201b28
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-02-20 13:55:50 -0800

    Added Blackjack score method

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -3,5 +3,27 @@
 VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
 
 def blackjack_score(hand)
-
+    score = 0
+    hand.each do |i|
+        # puts i
+        if (i == "King") || (i == "Queen") || (i == "Jack")
+            i = 10
+        end
+        # puts i
+        score += i
+    end
+    return score
 end
+
+# def blackjack_score(facecards)
+    # facecards.each_value {|value| puts value } 
+    # hand.each_value do |value|
+    #     score += value
+    # end
+# end
+
+facecards = ["King", "Queen", "Jack"]
+hand = [3, 4]
+
+# puts blackjack_score(hand)
+puts blackjack_score(hand)
