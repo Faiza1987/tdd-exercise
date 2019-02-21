7facec83f10521b02ec3b6be6bba1d57d743b68d
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-02-20 23:13:25 -0800

    Modifies blackjack.rb

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,29 +1,32 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
 
 def blackjack_score(hand)
     score = 0
+    if hand.length > 5 
+        raise ArgumentError, "Cards in hand are more than the allowed number."
+    end
+
     hand.each do |i|
-        # puts i
         if (i == "King") || (i == "Queen") || (i == "Jack")
             i = 10
         end
-        # puts i
+
+        if (i == "Ace") && (score >= 20) 
+            i = 1
+        end
+        
         score += i
     end
+    # puts score
     return score
 end
 
-# def blackjack_score(facecards)
-    # facecards.each_value {|value| puts value } 
-    # hand.each_value do |value|
-    #     score += value
-    # end
-# end
-
 facecards = ["King", "Queen", "Jack"]
 hand = [3, 4]
+ace = [10, 10, "Ace"]
 
+puts blackjack_score(ace)
 puts blackjack_score(hand)
 puts blackjack_score(facecards)
